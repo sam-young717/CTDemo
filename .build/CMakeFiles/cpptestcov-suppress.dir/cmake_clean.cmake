56ba14ed0f717685fb09b75d4f140ba52bd9bcd3
sam-young717 <young.sam17@yahoo.com>
2025-04-01 09:53:41 -0400

    removing build

--- .build/CMakeFiles/cpptestcov-suppress.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cpptestcov-suppress"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cpptestcov-suppress.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- .build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/syoung/Documents/LastData/cpptest-ct/examples/ATM
-# Build directory: /mnt/c/Users/syoung/Documents/LastData/cpptest-ct/examples/ATM/.build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(atm_gtest "/mnt/c/Users/syoung/Documents/LastData/cpptest-ct/examples/ATM/.build/atm_gtest")
-set_tests_properties(atm_gtest PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/syoung/Documents/LastData/cpptest-ct/examples/ATM/CMakeLists.txt;73;add_test;/mnt/c/Users/syoung/Documents/LastData/cpptest-ct/examples/ATM/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
