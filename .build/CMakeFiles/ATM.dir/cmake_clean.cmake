48fbe6a4ba68835ee66b005534de4fbc5c8d4b33
sam-young717 <young.sam17@yahoo.com>
2025-04-03 13:04:25 -0400

    test

---
+++ .build/CMakeFiles/ATM.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ATM.dir/ATM.cpp.o"
+  "CMakeFiles/ATM.dir/ATM.cpp.o.d"
+  "CMakeFiles/ATM.dir/Account.cpp.o"
+  "CMakeFiles/ATM.dir/Account.cpp.o.d"
+  "CMakeFiles/ATM.dir/Bank.cpp.o"
+  "CMakeFiles/ATM.dir/Bank.cpp.o.d"
+  "CMakeFiles/ATM.dir/BaseDisplay.cpp.o"
+  "CMakeFiles/ATM.dir/BaseDisplay.cpp.o.d"
+  "libATM.a"
+  "libATM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
